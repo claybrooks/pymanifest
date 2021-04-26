bedcb866b7b1aac30d8a0dff521fe90102422527
Clay Brooks <clay_brooks@outlook.com>
2021-04-26 19:16:29 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# pymanifest
+# PyManifest
+
+## Requirements
+
+* Python 3.0+
+* Pip
+
+> If you're having any troubles with installation, this guide may help you: https://github.com/BurntSushi/nfldb/wiki/Python-&-pip-Windows-installation
+
+## Install & Run
+
+```
+pip install pymanifest
+python -m pymanifest --directory /path/to/include --ignore-directory /path/to/exclude --file /path/to/single/file
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pymanifest',
+    version='1.0',
+    packages=['pymanifest'],
+    author="Clay Brooks",
+    author_email="clay_brooks@outlook.com",
+    description="File list utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/claybrooks/pymanifest",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Operating System :: OS Independent",
+    ],
+)
